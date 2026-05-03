d23fab09f5525252bd61a76b1618b03b4a39dd79
jacknicolas345-cmyk <jacknicolas345@gmail.com>
2026-05-03 13:12:06 +0330

    Initialize App component with routing and context

---
+++ app/frontend/src/App.js
@@ -0,0 +1,66 @@
+"import React from \"react\";
+import \"@/App.css\";
+import { BrowserRouter, Routes, Route } from \"react-router-dom\";
+import { Toaster } from \"sonner\";
+import { AuthProvider } from \"./lib/AuthContext\";
+import { CartProvider } from \"./lib/CartContext\";
+import Navbar from \"./components/Navbar\";
+import Footer from \"./components/Footer\";
+import { ProtectedRoute, AdminRoute } from \"./components/RouteGuards\";
+
+import Home from \"./pages/Home\";
+import Shop from \"./pages/Shop\";
+import ProductDetail from \"./pages/ProductDetail\";
+import Cart from \"./pages/Cart\";
+import Checkout from \"./pages/Checkout\";
+import CheckoutSuccess from \"./pages/CheckoutSuccess\";
+import Login from \"./pages/Login\";
+import Register from \"./pages/Register\";
+import Orders from \"./pages/Orders\";
+import Account from \"./pages/Account\";
+import About from \"./pages/About\";
+
+import AdminLayout from \"./pages/admin/AdminLayout\";
+import AdminDashboard from \"./pages/admin/AdminDashboard\";
+import AdminProducts from \"./pages/admin/AdminProducts\";
+import AdminOrders from \"./pages/admin/AdminOrders\";
+import AdminUsers from \"./pages/admin/AdminUsers\";
+
+function App() {
+  return (
+    <div className=\"App\" dir=\"rtl\">
+      <BrowserRouter>
+        <AuthProvider>
+          <CartProvider>
+            <Navbar />
+            <Routes>
+              <Route path=\"/\" element={<Home />} />
+              <Route path=\"/shop\" element={<Shop />} />
+              <Route path=\"/product/:id\" element={<ProductDetail />} />
+              <Route path=\"/cart\" element={<Cart />} />
+              <Route path=\"/checkout\" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
+              <Route path=\"/checkout/success\" element={<CheckoutSuccess />} />
+              <Route path=\"/login\" element={<Login />} />
+              <Route path=\"/register\" element={<Register />} />
+              <Route path=\"/about\" element={<About />} />
+              <Route path=\"/orders\" element={<ProtectedRoute><Orders /></ProtectedRoute>} />
+              <Route path=\"/account\" element={<ProtectedRoute><Account /></ProtectedRoute>} />
+
+              <Route path=\"/admin\" element={<AdminRoute><AdminLayout /></AdminRoute>}>
+                <Route index element={<AdminDashboard />} />
+                <Route path=\"products\" element={<AdminProducts />} />
+                <Route path=\"orders\" element={<AdminOrders />} />
+                <Route path=\"users\" element={<AdminUsers />} />
+              </Route>
+            </Routes>
+            <Footer />
+            <Toaster position=\"top-center\" richColors />
+          </CartProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+"
